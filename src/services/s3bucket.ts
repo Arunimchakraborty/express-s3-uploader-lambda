f7e6323dfb5cfe161e2371c854c4e1fa5c8f62a0
Arunim Chakraborty <arunimchakraborty@gmail.com>
2023-10-30 20:05:34 +0530

    s3bucket file indentation

--- src/services/s3bucket.ts
+++ src/services/s3bucket.ts
@@ -7,16 +7,16 @@ const { bucketName } = s3bucket;
 
 const s3Client = new S3Client({
 	region: "ap-south-1",
-  credentials: {
-    accessKeyId: access_key,
-    secretAccessKey: secret_key
-  }
+	credentials: {
+		accessKeyId: access_key,
+		secretAccessKey: secret_key,
+	},
 });
 
 // console.log(await s3Client.config.region)
 
 async function uploadToS3(key: string, body: any) {
-  console.log(`Uploading file ${key} to S3Bucket`)
+	console.log(`Uploading file ${key} to S3Bucket`);
 	await s3Client.send(
 		new PutObjectCommand({
 			Bucket: bucketName,
@@ -24,7 +24,7 @@ async function uploadToS3(key: string, body: any) {
 			Body: body,
 		})
 	);
-  console.log(`Uploaded file ${key} to bucket`)
+	console.log(`Uploaded file ${key} to bucket`);
 }
 
 export default { uploadToS3 };
